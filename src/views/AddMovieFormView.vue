c6650b31fa9d26096204ffd8bff51be2bf43f607
Hayden Byfield <hayden@email.com>
2026-04-13 03:55:12 -0500

    Small change to MovieForm.vue

--- src/views/AddMovieFormView.vue
+++ src/views/AddMovieFormView.vue
@@ -1,23 +1,6 @@
 <script setup>
 import MovieForm from '../components/MovieForm.vue';
-import { onMounted, ref } from 'vue'
 
-const movies = ref([])
-
-onMounted(async () =>{
-    try {
-        let response = await fetch('/api/v1/movies')
-        let data = await response.json()
-        movies.value = data.movies
-        console.log(data)
-    } catch (error) {
-        console.error(error);
-    }
-})
-
-const updateMovie = (movie) => {
-    movies.value.push(movie)
-}
 </script>
 
 <template>
